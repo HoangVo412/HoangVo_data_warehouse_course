ce64d7ebcdd373f59e4320ee1b0f6af09eb76794
HoangVo <vohoang41291@gmail.com>
2022-09-17 16:43:32 +0700

    add stock_item_id

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -8,11 +8,10 @@ Yêu cầu #0103b:
 
 #}
 
-
-
 SELECT 
   order_line_id AS sales_order_line_id
   , quantity
   , unit_price
   , quantity * unit_price AS gross_amount
+  , stock_item_id AS product_id
 FROM `duckdata-320210.wide_world_importers.sales__order_lines`
