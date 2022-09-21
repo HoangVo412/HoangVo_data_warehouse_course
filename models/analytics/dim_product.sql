fa23c9b00610d3982cd1c7debcab2cf8391569cc
HoangVo <vohoang41291@gmail.com>
2022-09-21 21:28:27 +0700

    CASE WHEN for is_chiller_stock

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -37,8 +37,9 @@ SELECT
   , dim_supplier.supplier_name
   , 
   CASE
-  WHEN dim_product.is_chiller_stock IS TRUE THEN 'This stock item needs to be in a chiller'
-  WHEN dim_product.is_chiller_stock IS FALSE THEN 'This stock item does not need to be in a chiller'
+  WHEN dim_product.is_chiller_stock IS TRUE THEN 'Chiller Stock'
+  WHEN dim_product.is_chiller_stock IS FALSE THEN 'Not Chiller Stock'
+  ELSE 'Unknown'
   END AS is_chiller_stock
 FROM dim_product__cast_type AS dim_product
 LEFT JOIN {{ref('dim_supplier')}}
