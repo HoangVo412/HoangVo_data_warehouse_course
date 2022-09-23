3e5c4595037dace0f794b7430292032196767399
HoangVo <vohoang41291@gmail.com>
2022-09-23 20:15:47 +0700

    flatten du lieu cho bang dim_customer

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -18,14 +18,19 @@ WITH dim_customer__source AS (
 
 , dim_customer__cast_type AS (
   SELECT 
-    CAST(customer_id AS INTEGER) AS customer_id
-    , CAST(customer_name AS STRING) AS customer_name
+    CAST(customer_category_id AS INTEGER) AS customer_category_id
+    , CAST(customer_category_name AS STRING) AS customer_category_name
+    , CAST(buying_group_id AS INTEGER) AS buying_group_id
+    , CAST(buying_group_name AS STRING) AS buying_group_name
+    , CAST(delivery_method_id AS INTEGER) AS delivery_method_id
+    , CAST(delivery_method_name AS STRING) AS delivery_method_name
   FROM dim_customer__source
 )
 
-
 SELECT 
-  customer_id
+  dim_customer.customer_category_id
+  , dim_customer.customer_category_name
+  , dim_customer.
   , customer_name 
-FROM dim_customer__cast_type
+FROM dim_customer__cast_type AS dim_customer
 
