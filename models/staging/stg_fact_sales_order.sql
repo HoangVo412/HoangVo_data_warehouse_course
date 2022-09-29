035b370ab85dff6e12035f4a8b3946a12c141b89
HoangVo <vohoang41291@gmail.com>
2022-09-29 21:09:51 +0700

    create data model stg_fact_sales_order

---
+++ models/staging/stg_fact_sales_order.sql
@@ -0,0 +1,21 @@
+WITH stg_fact_sales_order__source AS (
+  SELECT *
+  FROM `duckdata-320210.wide_world_importers.sales__orders`
+)
+
+, stg_fact_sales_order__rename_column AS (
+  SELECT
+    order_id AS sales_order_id
+    , customer_id AS customer_id
+  FROM stg_fact_sales_order__source
+)
+
+, stg_fact_sales_order__cast_type AS (
+  SELECT
+    CAST(sales_order_id AS INTEGER) AS sales_order_id
+    , CAST(customer_id AS INTEGER) AS customer_id
+  FROM stg_fact_sales_order__rename_column
+)
+
+SELECT *
+FROM stg_fact_sales_order__cast_type
