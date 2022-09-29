a457f855fb8ad0f32c28399aea2d9d9b8f719738
HoangVo <vohoang41291@gmail.com>
2022-09-29 21:27:23 +0700

    LEFT JOIN fact_sales_order_line AND stg_sales_order

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -6,6 +6,7 @@ WITH fact_sales_order_line__source AS(
 , fact_sales_order_line__rename_column AS (
   SELECT
     order_line_id AS sales_order_line_id
+    , order_id AS order_id
     , stock_item_id AS product_id
     , quantity AS quantity
     , unit_price AS unit_price  
@@ -15,6 +16,7 @@ WITH fact_sales_order_line__source AS(
 , fact_sales_order_line__cast_type AS (
   SELECT
     CAST(sales_order_line_id AS INTEGER) AS sales_order_line_id
+    , CAST(order_id AS INTEGER) AS order_id
     , CAST(product_id AS INTEGER) AS product_id
     , CAST(quantity AS INTEGER) AS quantity
     , CAST(unit_price AS NUMERIC) AS unit_price
@@ -28,5 +30,9 @@ WITH fact_sales_order_line__source AS(
   FROM fact_sales_order_line__cast_type
 )
 
-SELECT * 
+SELECT 
+  *
+  , stg_fact_sales_order.customer_id
 FROM fact_sales_order_line__calculate
+LEFT JOIN {{ref('stg_fact_sales_order')}} AS stg_fact_sales_order
+ON fact_sales_order_line__calculate.order_id = stg_fact_sales_order.sales_order_id
