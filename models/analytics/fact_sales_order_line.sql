71a2f8f6ae713a5ebd4f7a8d31706bf913798fda
HoangVo <vohoang41291@gmail.com>
2022-09-29 20:47:19 +0700

    CAST type fact_sales_order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -8,15 +8,24 @@ WITH fact_sales_order_line__source AS(
     order_line_id AS sales_order_line_id
     , stock_item_id AS product_id
     , quantity AS quantity
-    , unit_price AS unit_price
+    , unit_price AS unit_price  
   FROM fact_sales_order_line__source
+)
+
+, fact_sales_order_line__cast_type AS (
+  SELECT
+    CAST(sales_order_line_id AS INTEGER) AS sales_order_line_id
+    , CAST(product_id AS INTEGER) AS product_id
+    , CAST(quantity AS INTEGER) AS quantity
+    , CAST(unit_price AS NUMERIC) AS unit_price
+  FROM fact_sales_order_line__rename_column
 )
 
 , fact_sales_order_line__calculate AS (
   SELECT 
     *
     , quantity * unit_price AS gross_amount
-  FROM fact_sales_order_line__rename_column
+  FROM fact_sales_order_line__cast_type
 )
 
 SELECT * 
