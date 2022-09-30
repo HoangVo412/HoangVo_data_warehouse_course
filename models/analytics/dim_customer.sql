b23d1c8251bca1103f6cbe969446f8d72b7ccad9
HoangVo <vohoang41291@gmail.com>
2022-09-30 21:22:32 +0700

    left justify WHEN ELSE

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -7,8 +7,12 @@ WITH dim_customer__source AS (
   SELECT
     CAST(customer_id AS INTEGER) AS customer_id
     , CAST(customer_name AS STRING) AS customer_name
+    , CAST(customer_category_id AS INTEGER) AS customer_category_id
+    , CAST(buying_group_id AS INTEGER) AS buying_group_id
+    , CAST(delivery_method_id AS INTEGER) AS delivery_method_id
   FROM dim_customer__source 
 )
 
 SELECT *
-FROM dim_cusomer__cast_type
+FROM dim_cusomer__cast_type AS dim_customer
+LEFT JOIN {{ref('stg_factc')}}
