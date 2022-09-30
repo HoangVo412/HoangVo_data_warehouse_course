a29758c23b794f44ae4d4ca824baca0e5b122d7f
HoangVo <vohoang41291@gmail.com>
2022-09-30 21:07:25 +0700

    left justify SELECT FROM

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,13 +1,13 @@
 WITH dim_supplier__source AS (
-    SELECT *
-    FROM `duckdata-320210.wide_world_importers.purchasing__suppliers`
+  SELECT *
+  FROM `duckdata-320210.wide_world_importers.purchasing__suppliers`
 )
 
 , dim_supplier__cast_type AS (
-    SELECT
-      CAST(supplier_id AS INTEGER) AS supplier_id
-      , CAST(supplier_name AS STRING) AS supplier_name
-    FROM dim_supplier__source
+  SELECT
+    CAST(supplier_id AS INTEGER) AS supplier_id
+    , CAST(supplier_name AS STRING) AS supplier_name
+  FROM dim_supplier__source
 )
 
 SELECT *
