3edaf58fb89fea39eb22c20eb25506281d9f962f
HoangVo <vohoang41291@gmail.com>
2022-09-30 20:58:16 +0700

    LEFT JOIN dim_products and dim_suppliers

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -8,6 +8,7 @@ WITH dim_product__source AS (
     stock_item_id AS product_id
     , stock_item_name AS product_name
     , brand AS brand_name
+    , supplier_id AS supplier_id
   FROM dim_product__source
 )
 
@@ -16,8 +17,16 @@ WITH dim_product__source AS (
     CAST(product_id AS INTEGER) AS product_id
     , CAST(product_name AS STRING) AS product_name
     , CAST(brand_name AS STRING) AS brand_name
+    , CAST(supplier_id AS INTEGER) AS supplier_id
   FROM dim_product__rename_column
 )
 
-SELECT *
-FROM dim_product__cast_type
+SELECT 
+  dim_product.product_id
+  , dim_product.product_name
+  , dim_product.brand_name
+  , dim_product.supplier_id
+  , dim_suppliers.supplier_name
+FROM dim_product__cast_type AS dim_product
+LEFT JOIN {{ref('dim_suppliers')}} AS dim_suppliers
+ON dim_product.supplier_id = dim_suppliers.supplier_id
