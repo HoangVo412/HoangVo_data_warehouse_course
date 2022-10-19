37f4af1cd2efd7b5f387ceba4d36c6c4219906e3
HoangVo <vohoang41291@gmail.com>
2022-10-20 05:34:50 +0700

    change name CTE

--- models/analytics/fact_inventory_snapshot.sql
+++ models/analytics/fact_inventory_snapshot.sql
@@ -1,45 +1,45 @@
-with cte1 as (
-select 
-    cast(date_trunc(transaction_occurred_when, MONTH) as date) as month
+WITH fact_inventory_snapshot__net_change_quantity AS (
+  SELECT 
+    CAST(DATE_TRUNC(transaction_occurred_when, MONTH) AS DATE) AS year_month
     , product_id 
-    , sum(quantity) as net_change_quantity
-  from `main-363923.wide_world_importers_dwh.fact_stock_item_transaction`
-  group by month, product_id
+    , SUM(quantity) AS net_change_quantity
+  FROM `main-363923.wide_world_importers_dwh.fact_stock_item_transaction`
+  GROUP BY year_month, product_id
 )
 
-, cte2 as (
-  select 
-  cast(date_trunc(order_date, MONTH) as date) as month
-  , product_id
-  , sum(quantity) as sold_quantity
-from `main-363923.wide_world_importers_dwh.fact_sales_order_line`
-group by month, product_id
+, fact_invetory_snapshot__sold_quantity AS (
+  SELECT 
+    CAST(DATE_TRUNC(order_date, MONTH) AS DATE) AS year_month
+    , product_id
+    , SUM(quantity) AS sold_quantity
+  FROM `main-363923.wide_world_importers_dwh.fact_sales_order_line`
+  GROUP BY year_month, product_id
 )
 
-, cte3 as (
-select distinct
-  year_month as month
-  , product_id
-from `main-363923.wide_world_importers_dwh.dim_date`
-cross join `main-363923.wide_world_importers_dwh.fact_stock_item_transaction`
+, fact_inventory_snapshot__distinct_product_id AS (
+  SELECT DISTINCT
+    year_month
+    , product_id
+  FROM `main-363923.wide_world_importers_dwh.dim_date`
+  CROSS JOIN `main-363923.wide_world_importers_dwh.fact_stock_item_transaction`
 )
 
-, cte4 as (
-select
-  cte3.month
-  , cte3.product_id
-  , coalesce(cte1.net_change_quantity, 0) as net_change_quantity
-  , coalesce(cte2.sold_quantity, 0) as sold_quantity
-from cte3
-left join cte1
-  on cte3.month = cte1.month
-  and cte3.product_id = cte1.product_id
-left join cte2
-  on cte3.month = cte2.month
-  and cte3.product_id = cte2.product_id
+, fact_inventory_snapshot__handle_null AS (
+  SELECT
+    fact_inventory_snapshot__distinct_product_id.year_month
+    , fact_inventory_snapshot__distinct_product_id.product_id
+    , COALESCE(fact_inventory_snapshot__net_change_quantity.net_change_quantity, 0) AS net_change_quantity
+    , COALESCE(fact_invetory_snapshot__sold_quantity.sold_quantity, 0) AS sold_quantity
+  FROM fact_inventory_snapshot__distinct_product_id
+  LEFT JOIN fact_inventory_snapshot__net_change_quantity
+    ON fact_inventory_snapshot__distinct_product_id.year_month = fact_inventory_snapshot__net_change_quantity.year_month
+    AND fact_inventory_snapshot__distinct_product_id.product_id = fact_inventory_snapshot__net_change_quantity.product_id
+  LEFT JOIN fact_invetory_snapshot__sold_quantity
+    ON fact_inventory_snapshot__distinct_product_id.year_month = fact_invetory_snapshot__sold_quantity.year_month
+    AND fact_inventory_snapshot__distinct_product_id.product_id = fact_invetory_snapshot__sold_quantity.product_id
 )
 
-select
+SELECT
   *
-  , sum(cte4.net_change_quantity) over (partition by cte4.product_id order by cte4.month) as closing_on_hand
-from cte4
+  , SUM(fact_inventory_snapshot__handle_null.net_change_quantity) OVER (PARTITION BY fact_inventory_snapshot__handle_null.product_id ORDER BY     fact_inventory_snapshot__handle_null.year_month) AS closing_on_hand
+FROM fact_inventory_snapshot__handle_null
