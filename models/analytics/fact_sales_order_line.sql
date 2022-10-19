bad5edfa3f8f48f1f3ca1f86dc70fd157079cdde
HoangVo <vohoang41291@gmail.com>
2022-10-20 06:17:40 +0700

    rearrange column

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -34,11 +34,11 @@ SELECT
   fact_sales_order_line.sales_order_line_id
   , fact_sales_order_line.order_id
   , fact_sales_order_line.product_id
+  , stg_fact_sales_order.customer_id
+  , COALESCE(stg_fact_sales_order.picked_by_person_id, 0) AS picked_by_person_id
   , fact_sales_order_line.quantity
   , fact_sales_order_line.unit_price
   , fact_sales_order_line.gross_amount
-  , stg_fact_sales_order.customer_id
-  , COALESCE(stg_fact_sales_order.picked_by_person_id, 0) AS picked_by_person_id
   , stg_fact_sales_order.order_date
 FROM fact_sales_order_line__calculate AS fact_sales_order_line
 LEFT JOIN {{ref('stg_fact_sales_order')}} AS stg_fact_sales_order
