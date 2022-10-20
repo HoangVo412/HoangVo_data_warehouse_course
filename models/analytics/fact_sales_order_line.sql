b2dbf4ba91f4b0bdd686e0fe3f56984db1b116e7
HoangVo <vohoang41291@gmail.com>
2022-10-20 23:05:32 +0700

    rename order_id -> sales_order_id

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -6,7 +6,7 @@ WITH fact_sales_order_line__source AS(
 , fact_sales_order_line__rename_column AS (
   SELECT
     order_line_id AS sales_order_line_id
-    , order_id AS order_id
+    , order_id AS sales_order_id
     , stock_item_id AS product_id
     , quantity AS quantity
     , unit_price AS unit_price  
@@ -16,7 +16,7 @@ WITH fact_sales_order_line__source AS(
 , fact_sales_order_line__cast_type AS (
   SELECT
     CAST(sales_order_line_id AS INTEGER) AS sales_order_line_id
-    , CAST(order_id AS INTEGER) AS order_id
+    , CAST(sales_order_id AS INTEGER) AS sales_order_id
     , CAST(product_id AS INTEGER) AS product_id
     , CAST(quantity AS NUMERIC) AS quantity
     , CAST(unit_price AS NUMERIC) AS unit_price
@@ -32,7 +32,7 @@ WITH fact_sales_order_line__source AS(
 
 SELECT 
   fact_sales_order_line.sales_order_line_id
-  , fact_sales_order_line.order_id
+  , fact_sales_order_line.sales_order_id
   , fact_sales_order_line.product_id
   , stg_fact_sales_order.customer_id
   , COALESCE(stg_fact_sales_order.picked_by_person_id, 0) AS picked_by_person_id
@@ -42,4 +42,4 @@ SELECT
   , stg_fact_sales_order.order_date
 FROM fact_sales_order_line__calculate AS fact_sales_order_line
 LEFT JOIN {{ref('stg_fact_sales_order')}} AS stg_fact_sales_order
-ON fact_sales_order_line.order_id = stg_fact_sales_order.sales_order_id
+ON fact_sales_order_line.sales_order_id = stg_fact_sales_order.sales_order_id
