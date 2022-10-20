60bafbb5a074ab16f171c5d476304eb6d5fecc0b
HoangVo <vohoang41291@gmail.com>
2022-10-21 00:49:10 +0700

    add category_id + category_name from dim_product

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -36,10 +36,14 @@ SELECT
   , fact_sales_order_line.product_id
   , stg_fact_sales_order.customer_id
   , COALESCE(stg_fact_sales_order.picked_by_person_id, 0) AS picked_by_person_id
+  , dim_product.category_id
+  , dim_product.category_name
   , fact_sales_order_line.quantity
   , fact_sales_order_line.unit_price
   , fact_sales_order_line.gross_amount
   , stg_fact_sales_order.order_date
 FROM fact_sales_order_line__calculate AS fact_sales_order_line
 LEFT JOIN {{ref('stg_fact_sales_order')}} AS stg_fact_sales_order
-ON fact_sales_order_line.sales_order_id = stg_fact_sales_order.sales_order_id
+  ON fact_sales_order_line.sales_order_id = stg_fact_sales_order.sales_order_id
+LEFT JOIN {{ref('dim_product')}} AS dim_product
+  ON fact_sales_order_line.product_id = dim_product.product_id
